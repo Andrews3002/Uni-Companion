2c3681e6f7f806619d256945ce997761fffca540
Andrews3002 <alexangeloandrews@gmail.com>
2025-06-22 17:47:04 -0400

    main commit for base level before testing merge conflicts

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import customtkinter as ctk
 from PIL import Image
 
-#HomePage change
-
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title('Uni Companion')
         self.after(10, lambda: self.state('zoomed'))
-        
-        #courses page change 2
         
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
